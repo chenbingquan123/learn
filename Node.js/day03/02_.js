e5410f3bd2e6e75ddad68bb44cea4e5cdcbd3ac1
陈炳权 <337406374@qq.com>
2020-10-26 19:22:30 +0800

    notes

--- Node.js/day03/02_.js
+++ Node.js/day03/02_.js
@@ -1,6 +1,6 @@
-const { LOADIPHLPAPI } = require('dns');
+// const { LOADIPHLPAPI } = require('dns');
 const fs=require('fs');
-const { domainToUnicode } = require('url');
+// const { domainToUnicode } = require('url')
 
 // //1.查看文件的状态
 
@@ -42,7 +42,7 @@ const { domainToUnicode } = require('url');
 // let arr=fs.readdirSync('../day02');
 // console.log(arr);
 
-// fs.mkdir('../day02',(err)=>{
+// fs.mkdir('./mydir',(err)=>{
 //     if(err)  throw err;
 // });
 
@@ -59,7 +59,7 @@ const { domainToUnicode } = require('url');
 
 
 // // 练习：使用异步方法往文件3.txt中写入数据web2009
-// fs.writeFile('./03.txt','web2009',(err)=>
+// fs.writeFile('./03.txt','web2009',(err)=>{
 // if(err)   thorw  err;
 
 // 4.追加写入
@@ -70,9 +70,9 @@ const { domainToUnicode } = require('url');
 
 // //练习：声明变量堡村一组学生的数据，遍历数组得到每个姓名，同步方法写入到文件stu.txt中，每个名字占1行
 
-// let stu=['','','','','',];
+// let stu=['李莹男','胡飞','刘露','高洋洋','李天宝',];
 // for(let i=0;i<stu.length;i++){
-//     fs.appendFileSync('./stu.txt)',stu[]
+//     fs.appendFileSync('./stu.txt)',stu[i]+'\n');
 // }
 
 
@@ -82,7 +82,7 @@ const { domainToUnicode } = require('url');
 // fs.readFile('./stu.txt',(err,data)=>{
 //     if(err)  throw err;
 //     console.log(String (data));
-// })
+// });
 // //同步，读取3.txt
 // let  data=fs.readFileSync('./3.txt');
 // console.log(String (data));
@@ -100,20 +100,20 @@ const { domainToUnicode } = require('url');
 
 // 练习：如果文件2.txt存在，则同步删除文件，如果目录mydir2不存在，则同步创建该目录
 
-if(fs.existsSync('./2.txt')){
-    fs.unlinkSync('./2.txt'); 
-}
-if(fs.existsSync('./mydir2')){
-     fs.mkdirSync('./mydir2');
+// if(fs.existsSync('./2.txt')){
+//     fs.unlinkSync('./2.txt'); 
+// }
+// if(!fs.existsSync('./mydir2')){
+//      fs.mkdirSync('./mydir2');
 
-};
+// };
 
-8.//拷贝文件
-fs.copyFile('./3.txt','./mydir/a.txt',(err)=>{
-    if(err)  throw err;
-    console.log('拷贝成功');
-});
-//同步拷贝stu.txt到mydir2下
-fs.copyFileSync('./stu.txt','./mydir2/stu.txt');
+// 8.//拷贝文件
+// fs.copyFile('./3.txt','./mydir/a.txt',(err)=>{
+//     if(err)  throw err;
+//     console.log('拷贝成功');
+// });
+// //同步拷贝stu.txt到mydir2下
+// fs.copyFileSync('./stu.txt','./mydir2/stu.txt');
 
-console.log('运行结束');
+// console.log('运行结束');
